0d402c6f34e48515462c09b25ceca33b2a825adb
andrevich95 <andrew.yaroshevskyy@gmail.com>
2019-09-23 22:15:15 +0300

    Items update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,19 +21,19 @@
                         <img class="navbar-brand brand-logo" src="{{ asset('img/logo.png') }}" alt="Логотип">
                     </a>
                 </div>
-                <div class="col-md-8 pull-right">
+                <div class="col-md-6 pull-right">
                     <div class="row">
                         <form class="form-inline">
                             @csrf
                             <label>
                                 <span class="m-2">Логин</span>
-                                <input class="form-control" type="text" placeholder="Логин"/>
+                                <input class="form-control-sm" type="text" placeholder="Логин"/>
                             </label>
                             <label>
                                 <span class="m-2">Пароль</span>
-                                <input class="form-control" type="password" placeholder="Пароль"/>
+                                <input class="form-control-sm" type="password" placeholder="Пароль"/>
                             </label>
-                            <button class="btn ml-3 btn-danger" type="submit">
+                            <button class="btn btn-sm ml-3 btn-danger" type="submit">
                                 Войти
                             </button>
                         </form>
@@ -58,6 +58,10 @@
                         </ul>
                     </nav>
                 </div>
+                <div class="col-md-2">
+                    <p><i class="fa fa-phone mr-3"></i>099-734-999-1</p>
+                    <p><i class="fa fa-phone mr-3"></i>071-304-74-74</p>
+                </div>
             </div>
         </div>
     </header>
@@ -73,22 +77,24 @@
         <div class="container">
             <div class="footer__row">
                 <div class="section__phones">
-                    <p><i class="fa fa-phone-square footer__icon mr-3"></i>099-734-999-1</p>
-                    <p><i class="fa fa-phone-square footer__icon mr-3"></i>071-304-74-74</p>
+                    <p><i class="fa fa-phone footer__icon mr-3"></i>099-734-999-1</p>
+                    <p><i class="fa fa-phone footer__icon mr-3"></i>071-304-74-74</p>
                 </div>
                 <div class="section__addresses">
-                    <p><a href="mailto:citylinenet@gmail.com"><i class="fa fa-envelope footer__icon mr-3"></i>citylinenet@gmail.com</a></p>
-                    <p><i class="fa fa-map-marker footer__icon mr-3"></i>Донецкая область, г. Комсомолськое, ул. Ленина 12Е</p>
+                    <p><a href="mailto:citylinenet@gmail.com"><i class="fa fa-envelope footer__icon mr-3"></i>citylinenet@gmail.com</a>
+                    </p>
+                    <p><i class="fa fa-map-marker footer__icon mr-3"></i>Донецкая область, г. Комсомолськое, ул. Ленина
+                        12Е</p>
                 </div>
                 <div class="section__icons">
                     <div>
                         {{--<a class="footer__icon" href="http://instagram.com" target="_blank">--}}
-                            {{--<i class="fa fa-instagram">--}}
-                            {{--</i>--}}
+                        {{--<i class="fa fa-instagram">--}}
+                        {{--</i>--}}
                         {{--</a>--}}
                         {{--<a class="footer__icon" href="http://facebook.com" target="_blank">--}}
-                            {{--<i class="fa fa-facebook">--}}
-                            {{--</i>--}}
+                        {{--<i class="fa fa-facebook">--}}
+                        {{--</i>--}}
                         {{--</a>--}}
                         <a class="footer__icon" href="https://vk.com/citylinegroupe" target="_blank">
                             <i class="fa fa-vk">
